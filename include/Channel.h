b51c0b5775aa1fdfcbf093b74804b65660654012
pengfei.yan <pengfei.yan@shopee.com>
2026-03-15 06:20:15 +0000

    Implement Channel header file

--- include/Channel.h
+++ include/Channel.h
@@ -22,5 +22,58 @@ public:
     
     void handleEvent(Timestamp receiveTime);
 
-    void setReadCallback(ReadEventCallback cb) {}
+    // 设置回调函数对象
+    // std::move 并不是“移动值”，而是 把左值变成右值引用，右值引用允许资源从一个对象“搬走”而不是复制
+    void setReadCallback(ReadEventCallback cb) { readCallback_ = std::move(cb); }
+    void setWriteCallback(EventCallback cb) { writeCallback_ = std::move(cb); }
+    void setCloseCallback(EventCallback cb) { closeCallback_ = std::move(cb); }
+    void setErrorCallback(EventCallback cb) { errorCallback_ = std::move(cb); }
+
+    // 防止当channel被手动remove掉 channel还在执行回调操作
+    void tie(const std::shared_ptr<void> &);
+
+    int fd() const { return fd_; }
+    int events() const { return events_; }
+    void set_revents(int revt) { revents_ = revt; }
+
+    // 设置fd需要监听的事件状态 相当于epoll_ctl add delete
+    void enableReading() { events_ |= kReadEvent; update(); }
+    void disableReading() { events_ &= ~kReadEvent; update(); }
+    void enableWriting() { events_ |= kWriteEvent; update(); }
+    void disableWriting() { events_ &= ~kWriteEvent; update(); }
+    void disableAll() { events_ = kNoneEvent; update(); }
+
+    // 返回fd当前需要监听的事件状态
+    bool isNoneEvent() const { return events_ == kNoneEvent; }
+    bool isWriting() const { return events_ & kWriteEvent; }
+    bool isReading() const { return events_ & kReadEvent; }
+
+    int index() { return index_; }
+    void set_index(int idx) { index_ = idx; }
+
+    // one loop per thread
+    EventLoop *ownerLoop() { return loop_; }
+    void remove();
+private:
+    void update();
+    void handleEventWithGuard(Timestamp receiveTime);
+
+    static const int kNoneEvent;
+    static const int kReadEvent;
+    static const int kWriteEvent;
+
+    EventLoop *loop_; // 事件循环
+    const int fd_;    // fd，Poller监听的对象
+    int events_;      // 注册fd感兴趣的事件
+    int revents_;     // Poller返回的具体发生的事件
+    int index_;
+
+    std::weak_ptr<void> tie_;
+    bool tied_;
+
+    // 因为channel通道里可获知fd最终发生的具体的事件events，所以它负责调用具体事件的回调操作
+    ReadEventCallback readCallback_;
+    EventCallback writeCallback_;
+    EventCallback closeCallback_;
+    EventCallback errorCallback_;
 };
